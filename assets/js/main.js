97efd43980a1fe4b83a3331324c7de7bd5f8d077
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2025-04-03 16:49:34 +0200

    Novo template 2.0

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/*!
+=========================================================
+* Adelson Landing page
+=========================================================
+
+* Copyright: 2025 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com & Adelson Saguate
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+
+    $(document).on("click", "#baixar_cv", function (e) {
+        e.preventDefault();
+        window.open("assets/cv/Adelson_Manuel_Saguate_CV.pdf", "_blank");
+      });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
